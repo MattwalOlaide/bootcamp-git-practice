d9377e41429b7ca899ae0c8131b2a0da441ccd81
Olaide Ojewale <ojewaleolaide@gmail.com>
2015-07-07 10:12:23 +0100

    Implementation of unique function

--- sample.py
+++ sample.py
@@ -3,7 +3,12 @@ def unique(l):
 	'''
 	This function generate a list of unique values from l
 	'''
-	pass
+	new_list = []
+	for item in l:
+		if item not in new_list:
+			new_list.append(item)
+	return new_list
+	
 
 def reverse(s):
 	'''
